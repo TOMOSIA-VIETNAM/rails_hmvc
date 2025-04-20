136b11912f55a47084b45ea89075809a08e93b48
Minh Tang Q <minh.tang1@tomosia.com>
2025-04-20 22:17:22 +0700

    update template operator

--- lib/generators/hmvc/operation/operation_generator.rb
+++ lib/generators/hmvc/operation/operation_generator.rb
@@ -7,6 +7,7 @@ module RailsHmvc
 
       class_option :parent, type: :string, desc: 'Parent operation class'
       class_option :type, type: :string, desc: 'Project type (api/web)'
+      class_option :steps, type: :string, desc: 'List of step methods'
 
       def initialize(*args)
         super
@@ -35,6 +36,20 @@ module RailsHmvc
 
       def operation_class_name
         file_name.camelize
+      end
+
+      def steps
+        @options[:steps].to_s.split(',')
+      end
+
+      # def form_class_name
+      #   "#{namespace_name}::#{class_name.gsub('Operation', 'Form')}"
+      # end
+
+      def step_methods
+        steps.map do |method_name|
+          method_name.to_s.start_with?('step_') ? method_name.to_sym : :"step_#{method_name}"
+        end
       end
     end
   end
